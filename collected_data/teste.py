f82bc3e4a1a4a96cf72c7f74aaebe7c933ec8372
Alan Peterson <apeterson056@gmail.com>
2022-08-03 22:14:01 -0300

    arquivo de teste

---
+++ collected_data/teste.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+a = np.load("collected_data\gps_real_2.npy")
+b = np.load("collected_data\mpu_2.npy")
+c = np.load("collected_data\encoders.npy")
+d = np.load("collected_data\pose_real.npy")
+
+print("ok")
